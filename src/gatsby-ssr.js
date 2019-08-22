b0b07af0af16993451ca5d753b11331ff329a770
Gareth Bk <garethbk@gmail.com>
2019-08-22 10:50:19 -0700

    Initial commit

---
+++ src/gatsby-ssr.js
@@ -0,0 +1,28 @@
+const React = require('react')
+
+exports.onRenderBody = (
+  { setHeadComponents },
+  { zendeskKey, enableDuringDevelop = true },
+) => {
+  if (!enableDuringDevelop && process.env.NODE_ENV === 'development') {
+    console.log(
+      'enableDuringDevelo is set to false - gatsby-plugin-zendesk-chat will not load in development mode',
+    )
+    return null
+  }
+
+  if (!zendeskKey) {
+    console.log(
+      'No Zendesk key provided! gatsby-plugin-zendesk-chat will not load. Please add zendeskKey in gatsby-config.js',
+    )
+    return null
+  }
+
+  return setHeadComponents([
+    <script
+      id="ze-snippet"
+      key="gatsby-plugin-zendesk-chat"
+      src={`https://static.zdassets.com/ekr/snippet.js?key=${zendeskKey}`}
+    />,
+  ])
+}
